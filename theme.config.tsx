612a98065d6c67ee502e47f4d90d9c5ac9172089
iamClutchX <iamr.karan@gmail.com>
2023-02-07 15:08:59 +0530

    update

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from 'react';
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span style={{color: 'teal', fontSize:'36px', fontWeight: 'bold', }}>Code Articles</span>,
-
   project: {
+    icon: 'https://avatars.githubusercontent.com/u/77556288?s=200&v=4',
     link: 'https://github.com/iamClutchX/code-articles-x',
   },
   chat: {
+    icon: 'https://discord.com/assets/2c21aeda16de354ba5334551a883b481.png',
     link: 'https://discord.gg/YNThsCfjuA',
   },
   docsRepositoryBase: 'https://github.com/iamClutchX/code-articles-x',
   footer: {
+    component: 'footer',
     text: <span style={{color: 'teal', fontSize:'20px', fontWeight: 'bold',
     alignItems: 'center',
      display: 'flex',  justifyContent: 'center'}}>2023 © Code Articles</span>,
     
   },
-
-  // Google Analytics
   
 
 }
